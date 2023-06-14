e3c24f77658e6557a4f91fbecad5231f047e3b0c
golangdevm <honest.dedicate@gmail.com>
2023-06-14 15:46:50 -0700

    Fixing problem in the ListView with Stateful children using keys

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     super.key,
     required this.transaction,
@@ -11,6 +13,21 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  
+  Color? _bgColor;
+  
+  @override
+  void initState() {
+    const availableColors = [Colors.red, Colors.black, Colors.purple];
+    _bgColor =availableColors[Random().nextInt(3)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +39,19 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor ,
           radius: 30,
           child: Padding(
             padding: EdgeInsets.all(6),
-            child: FittedBox(
-                child: Text('\$${transaction.amount}')),
+            child: FittedBox(child: Text('\$${widget.transaction.amount}')),
           ),
         ),
         title: Text(
-          transaction.title!,
+          widget.transaction.title!,
           style: Theme.of(context).textTheme.titleSmall,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date!),
+          DateFormat.yMMMd().format(widget.transaction.date!),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
@@ -46,12 +63,12 @@ class TransactionItem extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               )
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).colorScheme.error,
-                onPressed: () => deleteTx(transaction.id),
+                onPressed: () => widget.deleteTx(widget.transaction.id),
               ),
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,12 +31,12 @@ class TransactionList extends StatelessWidget {
               ],
             );
           }))
-        : ListView.builder(
-            itemBuilder: (ctx, index) {
-              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
-            },
-            itemCount: transactions.length,
-          );
+        : ListView(children: 
+          transactions.map((tx) => TransactionItem(
+            key: ValueKey(tx.id),
+            transaction: tx, 
+            deleteTx: deleteTx)).toList()
+        ,);
   }
 }
 
